e915406a30ac3c4cb9a06b5db90bd8f1cb41ed1f
michaelmong <50898836+michaelmong@users.noreply.github.com>
2020-06-11 06:44:48 +0700

    1st optimized

--- UpdateLocation.js
+++ UpdateLocation.js
@@ -23,7 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://localhost:3000"); // update to match the domain you will make the request from
+//    res.header("Access-Control-Allow-Origin", "https://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://10.148.0.6:3000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
